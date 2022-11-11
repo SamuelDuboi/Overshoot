a0f6c7b93fab16f14ff452ba39d919d6df89538f
DUBOIS Samuel <s.dubois@rubika-edu.com>
2022-11-11 08:38:14 +0100

    player spawn pos

---
+++ OvershootProject/Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public List<Transform> spawnPoints = new List<Transform>();
+
+    public void OnSpawn(PlayerInput input)
+    {
+        input.gameObject.transform.position = spawnPoints[input.playerIndex].position;
+    }
+
+}
